ba8ebc8d3ea6a80ddf1571cb45d4aba2d3b43c6f
tomdaly <tomdaly@users.noreply.github.com>
2020-11-10 11:11:56 +0000

    Add screenshot function

--- test-app/cypress/integration/spec.ts
+++ test-app/cypress/integration/spec.ts
@@ -1,6 +1,9 @@
 describe('CRA', () => {
+  const takeScreenshot = () => cy.screenshot('screenshot', { scale: true });
+
   it('shows learn link', function () {
     cy.visit('http://localhost:3000')
+    takeScreenshot();
     cy.get('.App-link').should('be.visible')
       .and('have.text', 'Learn React')
   })
