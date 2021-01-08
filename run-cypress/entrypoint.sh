f8d2e310b72f42ddde945afff948989489c8736e
tomdaly <tomdaly@users.noreply.github.com>
2021-01-08 11:47:26 +0000

    Modify bash variable names

--- run-cypress/entrypoint.sh
+++ run-cypress/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash -l
 set -e
 
-## PRIVATE S3 INFO ##
-export MY_S3_ACCESS_KEY=notHere
-export MY_S3_SECRET_KEY=deletedFromIamDontCheckGitHistory
-#####################
-
 shopt -s extglob
-ROUTE=${INPUT_COMMENT/\/screenshot*([[:space:]])/}
-export CYPRESS_route=$ROUTE
-export CYPRESS_commentId=$INPUT_COMMENT_ID
-sh -c "echo Running Cypress with route $CYRESS_route, commentId $CYPRESS_commentId"
-# cd ../test-app/ || exit # uncomment line and comment below line to run locally
-cd /github/workspace/test-app || exit
+cd ../test-app/ || exit # uncomment line and comment below line to run locally
+# cd /github/workspace/test-app || exit
+route=${INPUT_COMMENT/\/screenshot*([[:space:]])/}
+export CYPRESS_ROUTE=$route
+export CYPRESS_COMMENT_ID=$INPUT_COMMENT_ID
+sh -c "echo Running Cypress with route $CYPRESS_ROUTE commentId $CYPRESS_COMMENT_ID"
 yarn install
 yarn test:cypress-server
 
--- test-app/cypress/integration/spec.ts
+++ test-app/cypress/integration/spec.ts
@@ -1,7 +1,7 @@
 describe('CRA', () => {
-  const route = Cypress.env('route') || "";
+  const route = Cypress.env('ROUTE') || "";
   const url = route // + "/index.html";
-  const fileName = 'screenshot-' + (Cypress.env('commentId') || "");
+  const fileName = 'screenshot-' + (Cypress.env('COMMENT_ID') || "");
   const takeScreenshot = () => cy.screenshot(fileName, { scale: true });
 
   it('shows main header', function () {
